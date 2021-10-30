27cf117a137325fa6f871a1654f684cc65aa5d5e
Ayush Kumar <kumarayush2104@gmail.com>
2021-10-30 21:56:37 +0530

    New: Program-8

---
+++ program-8.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <time.h>
+#include "include/enroll.h"
+#include "include/clear.h"
+
+main() {
+	clear();
+        enroll();
+	int num;
+	printf("------------------------------------\n");
+	printf("-	    Guessing Game	   -\n");
+	printf("------------------------------------\n");
+	printf("Guess an Integer: ");
+	scanf("%d", &num);
+	srand(time(NULL));
+	int randNum = rand() % 11;
+
+	if( randNum == num ) {
+		printf("You got me !!!\n");
+	} else {
+		printf("Sorry, The integer I was thinking is %d\n\n\n", randNum);
+	}
+
+}
