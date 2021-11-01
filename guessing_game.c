f45fe42ef61d38541c16c27f81218f5c07292b54
Ayush Kumar <kumarayush2104@gmail.com>
2021-11-01 20:00:53 +0530

    guessing_game: Added loop to run program infinitely, limited the integers to 0 to 10 and did some minor changes

--- guessing_game.c
+++ guessing_game.c
@@ -10,16 +10,20 @@ main() {
 	printf("------------------------------------\n");
 	printf("-	    Guessing Game	   -\n");
 	printf("------------------------------------\n");
-	printf("Guess an Integer: ");
+	printf("Guess an Integer in 0 to 10: ");
 	scanf("%d", &num);
 	srand(time(NULL));
 	int randNum = rand() % 11;
-
-	if( randNum == num ) {
-		printf("You got me !!!\n\n\n");
-	} else {
-		printf("Sorry, The integer I was thinking is %d\n\n\n", randNum);
+	if (num > 10) {
+		printf("You are supposed to write in 0 to 10\n\n");
+		exit(0);
 	}
-
-
+	else if (randNum == num) {
+		printf("You got me !!!\n\n");
+	} else {
+		printf("You are wrong, I was thinking about %d\n\n", randNum);
+	}
+	printf("Game will reset in 3 secs...\n\n");
+	sleep(2);
+	main();
 }
