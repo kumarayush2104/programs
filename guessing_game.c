aad4e265a688ee00fcc6ba2b4ac85d0fc16f5732
Ayush Kumar <kumarayush2104@gmail.com>
2021-11-02 21:06:52 +0530

    guessing_game: Fixed breaking of program if you enter any other alphabet

--- guessing_game.c
+++ guessing_game.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <time.h>
 #include "include/enroll.h"
 #include "include/clear.h"
@@ -7,14 +9,18 @@ main() {
 	clear();
         enroll();
 	int num;
+
 	printf("------------------------------------\n");
 	printf("-	    Guessing Game	   -\n");
 	printf("------------------------------------\n");
 	printf("Guess an Integer in 0 to 10: ");
-	scanf("%d", &num);
 	srand(time(NULL));
 	int randNum = rand() % 11;
-	if (num > 10) {
+	if (scanf("%d", &num) != 1) {
+		printf("You are supposed to write an integer\n\n");
+		exit(0);
+	}
+	else if (num > 10 || num <= 0) {
 		printf("You are supposed to write in 0 to 10\n\n");
 		exit(0);
 	}
@@ -23,6 +29,7 @@ main() {
 	} else {
 		printf("You are wrong, I was thinking about %d\n\n", randNum);
 	}
+
 	printf("Game will reset in 3 secs...\n\n");
 	sleep(2);
 	main();
