772174bf16d0885c9ac55195d6622c0c166f45c9
Ayush Kumar <kumarayush2104@gmail.com>
2021-11-14 10:15:51 +0000

    guessing_game: no need to exit the program because of unwanted input, will continue with warning

--- guessing_game.c
+++ guessing_game.c
@@ -10,24 +10,23 @@ main() {
         enroll();
 	int num;
 
-	printf("------------------------------------\n");
-	printf("-	    Guessing Game	   -\n");
-	printf("------------------------------------\n");
+	printf("----------------------------------\n");
+	printf("-	   Guessing Game  	 -\n");
+	printf("----------------------------------\n");
 	printf("Guess an Integer in 0 to 10: ");
 	srand(time(NULL));
 	int randNum = rand() % 11;
 	if (scanf("%d", &num) != 1) {
-		printf("You are supposed to write an integer\n\n");
-		exit(0);
+		scanf("%*[^\n]", &num);
+		printf("\nYou are supposed to write an integer\n\n");
 	}
 	else if (num > 10 || num < 0) {
-		printf("You are supposed to write in 0 to 10\n\n");
-		exit(0);
+		printf("\nYou are supposed to write in 0 to 10\n\n");
 	}
 	else if (randNum == num) {
-		printf("You got me !!!\n\n");
+		printf("\nYou got me !!!\n\n");
 	} else {
-		printf("You are wrong, I was thinking about %d\n\n", randNum);
+		printf("\nYou are wrong, I was thinking about %d\n\n", randNum);
 	}
 
 	printf("Game will reset in 3 secs...\n\n");
