c51cd50110bc6e233394cef4e82e5ef8ed25af1c
Ayush Kumar <kumarayush2104@gmail.com>
2021-11-16 14:49:21 +0530

    Fixed / modified few programs

--- program-7.c
+++ program-7.c
@@ -7,7 +7,7 @@ main() {
 	char name[100], college[300], course[100];
 	clear();
 	enroll();
-	printf("Enter Name: ");
+	printf("\nEnter Name: ");
 	scanf("%[^\n]s", name);
 	printf("Enter Enrollment Number: ");
 	scanf("%ld", &enrollNum);
@@ -16,8 +16,8 @@ main() {
 	printf("Enter Course name: ");
 	scanf("\n%[^\n]s", course);
 	printf("\n\n\n");
-	printf("---------------------------------------\n");
-        printf("-           Student Bio Data          -\n");
-        printf("---------------------------------------\n");
+	printf("------------------------------------------------------------\n");
+        printf("-           		Student Bio Data          	   -\n");
+        printf("------------------------------------------------------------\n");
 	printf("%s\n%ld\n%s\n%s\n\n", name, enrollNum, college, course);
 }
