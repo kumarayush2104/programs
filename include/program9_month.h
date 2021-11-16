5f79e6d76def605bd23ed37f1f5cd908b793ab6a
Ayush Kumar <kumarayush2104@gmail.com>
2021-11-16 15:07:23 +0000

    program 9: using pointer instead of fixed alloc size of string

--- include/program9_month.h
+++ include/program9_month.h
@@ -1,4 +1,4 @@
 // Declaring global variables //
 
 
-char getMonth[13][20] = { "January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+char *getMonth[20] = { "January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
