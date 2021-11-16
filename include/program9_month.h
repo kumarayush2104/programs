800d0a4157e2940c751b6a0d5346c096229592d3
Ayush Kumar <kumarayush2104@gmail.com>
2021-11-16 12:59:53 +0000

    Program-9: update

---
+++ include/program9_month.h
@@ -0,0 +1,4 @@
+// Declaring global variables //
+
+
+char getMonth[13][20] = { "January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
--- program-9.c
+++ program-9.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
+
 #include "include/clear.h"
 #include "include/enroll.h"
+#include "include/program9_month.h"
 
 main() {
 	clear();
-	int date, year;
-	char day[10]; char month[10];
+	int date,month, year;
+	char day[10];
         enroll();
-	printf("\nEnter day, followed by date (dd-mm-yyyy): ");
-	scanf("%s %d %s %d", day, &date, month, &year);
-//	printf("The day and date you have entered is:\nDate:- %d-%d-%d\nDay:- %s\n\n", date, month, year, day); **/
-
-	printf("Day: %s\nDate: %d\nMonth: %s\nYear: %d\n\n", day, date, month, year);
+	printf("\nEnter day, followed by date (eg: Wednesday 21-01-2021): ");
+	scanf("%s %d-%d-%d", day, &date, &month, &year);
+	printf("Day: %s\nDate: %d\nMonth: %s\nYear: %d\n\n", day, date, getMonth[--month], year);
 }
