b144210a2cfab45f3942635938e02271b8d58a12
Ayush Kumar <kumarayush2104@gmail.com>
2021-12-02 15:12:08 +0530

    Extras

--- extras/test.c
+++ extras/test.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
 
 main() {
-	int try[] = { 1, 2, 3, 4, 5 }, j=0, key=0;
-	int n = sizeof(try)/sizeof(int);
-	printf("\n\n%d\n\n", n);
-	for(int i=0;i<n;i++){
-		if(try[j] == try[i]){
-			break;
+	system("clear");
+	for(int i=1;i!=101;++i){
+		if ( i%2 == 0 ){
+			printf("%d ", i);
 		}
-		j++;
 	}
-
-	printf("\n\n%d\n\n", j);
+	printf("\n\n");
 }
