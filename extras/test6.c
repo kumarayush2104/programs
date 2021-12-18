40cb4328565a6d4e8953180f6dd413f3b9c166f7
Ayush Kumar <kumarayush2104@gmail.com>
2021-12-18 11:44:51 +0530

    Updated

--- extras/test6.c
+++ extras/test6.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 main() {
-	int num=0, len=0, tmp;
+	int num=0, len=1, tmp=0;
 	printf("Largest subsequences-2\n\n");
 	while(num!=-1) {
+		tmp=num;
 		printf("Enter a number: ");
 		scanf("%d", &num);
+		if( num==-1 ) break;
+		else if( tmp>num ) len=1;
 		len++;
-		tmp=num;
-		if(tmp>num) len=1;
 	}
-	if(len=1) printf("%d\n", len);
-	else printf("%d\n", len-1);
+	printf("%d\n", len-1);
 	printf("\n\n");
 }
--- program-19.c
+++ program-19.c
@@ -2,14 +2,15 @@
 #include "include/enroll.h"
 
 void main() {
-	system("clear");
         enroll();
 	int num1, num2;
 	printf("Enter first number: ");
 	scanf("%d", &num1);
 	printf("Enter second number: ");
 	scanf("%d", &num2);
+	printf("\nNumbers before swapping %d, %d\n\n", num1, num2);
 	swap(num1,num2);
+	main();
 }
 
 
@@ -17,5 +18,5 @@ int swap(int num1, int num2) {
 	int tmp=num1;
 	num1=num2;
 	num2=tmp;
-	printf("\n\nNumbers after swapping is %d, %d\n\n", num1, num2);
+	printf("Numbers after swapping %d, %d\n\n", num1, num2);
 }
