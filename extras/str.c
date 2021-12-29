bc4671148013b1e98fa4d0488270e048d4cda2ba
Ayush Kumar <kumarayush2104@gmail.com>
2021-12-29 21:56:46 +0530

    Updated

---
+++ extras/str.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+
+main() {
+	char str1[4]="utu", str2[4]="utu";
+
+	printf("\n\nlength of str1: %d, length of str2: %d\n\n", strlen(str1), strlen(str2));
+	printf("Are they same ? ");
+	if(strcmp(str1, str2) ==0 ) printf("Yes\n"); else printf("No\n");
+}
