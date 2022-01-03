15ea300d8c30878c48322a27be36115b4f0741d0
Ayush Kumar <kumarayush2104@gmail.com>
2022-01-03 22:20:37 +0530

    extras/test01.c: Updated

--- extras/test01.c
+++ extras/test01.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 void main(void) {
-	int a[2]={ 9 } b[2]= { 8 };
+	int a[2]={ 9 }, b[2]= { 8 };
 	int *c=&a[0], *d=&b[0];
 	printf("\n\nSubtraction of %d and %d is %d\n\n", *c, *d,  *c-*d);
 }
