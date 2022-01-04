1785dd6f948b3a51d21c8ad725475c337df89a12
Ayush Kumar <kumarayush2104@gmail.com>
2022-01-04 15:12:24 +0530

    Update

--- extras/transMat.c
+++ extras/transMat.c
@@ -2,16 +2,16 @@
 
 main() {
     int mat[4][4], key=1;
-    
+
     for(int i=0;i<3;i++) {
         for(int j=0;j<3;j++) {
             printf("Enter element %d, %d: ", i+1, j+1);
             scanf("%d", &mat[i][j]);
         }
     }
-    
+
     printf("\n\n");
-    
+
     if(key==1) {
         for(int i=0;i<3;i++){
             printf("| ");
@@ -24,16 +24,11 @@ main() {
     }
 
     printf("\n\nTranspose of the matrix\n\n");
-    
     for(int i=0;i<3;i++) {
         printf("| ");
         for(int j=0;j<3;j++) {
-            if(i!=j) {
                 printf("%d ", mat[j][i]);
-            } else printf("%d ", mat[i][j]);
         }
-        
         printf("|\n");
     }
-    
 }
