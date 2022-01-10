ca46928ef44e0f19fc9c2af3d876948991c3493d
Ayush Kumar <40270440+kumarayush2104@users.noreply.github.com>
2022-01-10 11:52:41 +0530

    Create task2.c

---
+++ extras/task2.c
@@ -0,0 +1,22 @@
+//
+//  File: task2.c
+//  Ayush Kumar (202103103510253)
+//  CS-1
+//
+
+#include <stdio.h>
+
+void func(int *ptr, int size) {
+    printf("Numbers in user_array are: \n");
+    for(int i=0;i<=size;i++) printf("%d\n", *(ptr + i));
+}
+void main(void) {
+    int user_array[9], arr_size=sizeof(user_array)/sizeof(int);
+    
+    for(int i=0;i<=arr_size;i++) {
+        printf("Enter %d number: ", i+1);
+        scanf("%d", &user_array[i]);
+    }
+    
+    func(&user_array, arr_size);
+}
