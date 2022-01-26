212da58929a31789e8ab4b7b9da9c9391cbd3b16
Ayush Kumar <40270440+kumarayush2104@users.noreply.github.com>
2022-01-26 17:35:08 +0530

    Create test003.c

---
+++ extras/test003.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+void main(void) {
+    char name1[50], name2[50];
+    int large; // Gonna compare the size of both strings
+    printf("Enter First Name: ");
+    scanf("%[^\n]s", name1);
+    printf("Enter Second Name: ");
+    scanf("\n%[^\n]s", name2);
+    
+    if(strlen(name1) > strlen(name2)) large=strlen(name1);
+    else large=strlen(name2);
+    
+    printf("\n\n");
+    
+    for(int i=large;i>=0;i--) {
+        printf("%c %c\n", name1[i], name2[i]);
+    }
+}
