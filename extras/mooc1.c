552e983e756c0b00212550186528c0a505918b0c
Ayush Kumar <40270440+kumarayush2104@users.noreply.github.com>
2022-01-28 07:58:36 -0500

    Update mooc1.c

--- extras/mooc1.c
+++ extras/mooc1.c
@@ -10,7 +10,7 @@ void main(void) {
         char fetch[25];
         FILE *fp=fopen("data.txt", "r");
         FILE *buffer=fopen("enrollment.txt", "w");
-        while(fscanf(fp, "\n%[^\n]", fetch) != EOF ) {
+        while(fscanf(fp, "\n%[^\n]s", fetch) != EOF ) {
                 fprintf(buffer, "202103103510%c%c%c\n", fetch[8], fetch[9], fetch[10]);
         }
 	fclose(fp);
