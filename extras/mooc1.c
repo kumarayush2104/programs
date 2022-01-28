1a56d1105cf548cd3fd8458230f527f2fb1a9660
Ayush Kumar <40270440+kumarayush2104@users.noreply.github.com>
2022-01-28 07:07:53 -0500

    Create mooc1.c

---
+++ extras/mooc1.c
@@ -0,0 +1,18 @@
+// MOOC Assignment
+//
+// Ayush Kumar (202103103510253)
+// CS-1
+//
+
+#include <stdio.h>
+
+void main(void) {
+        char fetch[25];
+        FILE *fp=fopen("data.txt", "r");
+        FILE *buffer=fopen("enrollment.txt", "w");
+        while(fscanf(fp, "\n%[^\n]", fetch) != EOF ) {
+                fprintf(buffer, "202103103510%c%c%c\n", fetch[8], fetch[9], fet>
+        }
+	fclose(fp);
+        fclose(buffer);
+}
