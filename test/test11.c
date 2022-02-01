0e0bbc4c4ba2800a1d7fec31d9e5a178ab585a76
Ayush Kumar <40270440+kumarayush2104@users.noreply.github.com>
2022-02-01 17:29:27 +0530

    Create test11.c

---
+++ test/test11.c
@@ -0,0 +1,15 @@
+#include <stdio>
+
+void main() {
+  int num=24;
+  printf("Value of num is %d\n\n", num);
+  
+  {
+    int num=90;
+    printf("Value of num is %d\n\n\n", num);
+  }
+  
+  int num=68;
+  printf("Value of num is %d\n\n\n", num);
+  
+}
