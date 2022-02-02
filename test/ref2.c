55b02fe811db5f17f997c55a52cfead5a784e0ab
Ayush Kumar <40270440+kumarayush2104@users.noreply.github.com>
2022-02-02 11:44:52 +0530

    Fixed ref2.c

--- test/ref2.c
+++ test/ref2.c
@@ -3,5 +3,6 @@
 void main() {
 	int soc[]= { 1,2,3,4,5,6,7,8,9,10 };
 	int *socptr= &soc;
-	printf("\n\nElements of soc[]\n: ");
+	printf("\n\nElements of soc[]:\n");
+	for(int i=0;i<10;i++) printf("%d\n", *(socptr + i)); 
 }
