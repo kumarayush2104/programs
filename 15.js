a1dfaae3c0d7d0a4e54c5cd1699a64bb8eaa8f09
Ayush Kumar <40270440+kumarayush2104@users.noreply.github.com>
2022-04-25 22:08:11 +0530

    Refreshing HTML branch: WIP

--- 15.js
+++ 15.js
@@ -1,8 +1,19 @@
 let num1=0, num2=0, def=1, met=0, allow=1;
 
+function res() {
+    size = document.getElementsByClassName("pri-button");
+    for (let i = 0; i < size.length; i++) {
+        document.getElementsByClassName("pri-button")[i].removeAttribute("style");
+    }
+    for (let i = 0; i < 2; i++) {
+        document.getElementsByClassName("spe-button-2")[i].removeAttribute("style");
+    }
+    
+}
+
 function num(value) {
     targ = document.getElementById("textview");
-    if(def==1){
+    if(def==1 && targ.textContent.indexOf(".") == -1){
         targ.innerHTML = value.toString();
         def=0;
     } else {
@@ -16,23 +27,36 @@ function opr(metin) {
     targ = document.getElementById("textview");
     if(def==0) {
         num1 = parseFloat(targ.textContent);
-        met= metin;
+        met = metin;
         def=1;
         allow=1;
         targ.innerHTML = "Second number";
     }
+    if(metin=="+"){
+        document.getElementsByClassName("spe-button-2")[0].setAttribute("style", "color: black; background: pink;")
+    }
+    if(metin=="-"){
+        document.getElementsByClassName("pri-button")[3].setAttribute("style", "color: black; background: pink;")
+    }
+    if(metin=="*"){
+        document.getElementsByClassName("pri-button")[2].setAttribute("style", "color: black; background: pink;")
+    }
+    if(metin=="/"){
+        document.getElementsByClassName("pri-button")[1].setAttribute("style", "color: black; background: pink;")
+    } 
 }
 
 function spe(metin) {
     targ = document.getElementById("textview");
     if(metin=="C") {
-            targ.innerHTML = "Enter a number";
-            num1=0;
-            num2=0;
-            def=1;
-            met=0;
+        res()
+        targ.innerHTML = "Enter a number";
+        num1=0;
+        num2=0;
+        def=1;
+        met=0;
     }
-    if(metin="P" && def==0) {
+    if(metin="P" && ( def == 0  || targ.textContent == "0")) {
         if(targ.innerHTML.indexOf(".") == -1) {
             targ.innerHTML += ".";
         }
@@ -40,6 +64,7 @@ function spe(metin) {
 }
 
 function trig(met, num1) {
+    res();
     targ = document.getElementById("textview");
     if(allow==1){
         if(met=="+") {
@@ -81,7 +106,14 @@ function jhero() {
 
 function bak() {
     targ = document.getElementById("textview");
-    text = targ.textContent;
-    targ.innerHTML = text.slice(0, -1);
-}
+    if(def==0) {
+        if(targ.textContent.length == 1 || targ.textContent.length == 0) {
+            targ.innerHTML = "0";
+        } else {
+            targ = document.getElementById("textview");
+            text = targ.textContent;
+            targ.innerHTML = text.slice(0, -1);
+        }
+    }
+}   
 
