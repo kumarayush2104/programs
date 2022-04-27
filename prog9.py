a0d1359907ff63bb97fa9f627dcd05d4ffb91293
Ayush Kumar <40270440+kumarayush2104@users.noreply.github.com>
2022-04-27 19:51:52 +0530

    Branch Refresh

--- prog9.py
+++ prog9.py
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-import os
-=======
->>>>>>> d7d037c9f56c6e7da260f26ceb10ad9d3f4e42ff
 class university:
     def __init__(self, name, year_of_estd, city):
         self.name = name
@@ -18,12 +14,7 @@ class professor(university):
         self.name_of_institute = name_of_institute
         super().__init__(name, year_of_estd, city)
     def display(self):
-<<<<<<< HEAD
-        os.system("clear")
-        print("Name:", self.name, "\nYear of Establishment:", self.year_of_estd, "\nCity:", self.city, "\nHighest Qualification:", self.highest_qualification, "\nArea of Research:", self.area_of_research, "\nYear of Experience:", self.year_of_experience, "\nYear of Joining:", self.year_of_joining, "\nName of Institute:", self.name_of_institute)
-=======
         print("\n\n\n\n\n\n\n\n\n\n\n\n\n\nName:", self.name, "\nYear of Establishment:", self.year_of_estd, "\nCity:", self.city, "\nHighest Qualification:", self.highest_qualification, "\nArea of Research:", self.area_of_research, "\nYear of Experience:", self.year_of_experience, "\nYear of Joining:", self.year_of_joining, "\nName of Institute:", self.name_of_institute)
->>>>>>> d7d037c9f56c6e7da260f26ceb10ad9d3f4e42ff
 class lab_assistant(university):
     designation = "Lab Assistant"
     def __init__(self, name, year_of_estd, city, highest_qualification, additional_skills, year_of_joining, name_of_institute):
@@ -33,12 +24,7 @@ class lab_assistant(university):
         self.name_of_institute = name_of_institute
         super().__init__(name, year_of_estd, city)
     def display(self):
-<<<<<<< HEAD
-        os.system("clear")
-        print("Name:", self.name, "\nYear of Establishment:", self.year_of_estd, "\nCity:", self.city,  "\nHighest Qualification:", self.highest_qualification, "\nAdditional Skills:", self.additional_skills, "\nYear of Joining:", self.year_of_joining, "\nName of Institute:", self.name_of_institute)
-=======
         print("\n\n\n\n\n\n\n\n\n\n\n\n\n\nName:", self.name, "\nYear of Establishment:", self.year_of_estd, "\nCity:", self.city,  "\nHighest Qualification:", self.highest_qualification, "\nAdditional Skills:", self.additional_skills, "\nYear of Joining:", self.year_of_joining, "\nName of Institute:", self.name_of_institute)
->>>>>>> d7d037c9f56c6e7da260f26ceb10ad9d3f4e42ff
 class office_assistant(university):
     designation = "Office Assistant"
     def __init__(self, name, year_of_estd, city, highest_qualification, year_of_joining, name_of_institute):
@@ -47,12 +33,7 @@ class office_assistant(university):
         self.name_of_institute = name_of_institute
         super().__init__(name, year_of_estd, city)
     def display(self):
-<<<<<<< HEAD
-        os.system("clear")
-        print("Name:", self.name, "\nYear of Establishment:", self.year_of_estd, "\nCity:", self.city, "\nHighest Qualification:", self.highest_qualification, "\nYear of Joining:", self.year_of_joining, "\nName of Institute:", self.name_of_institute)
-=======
         print("\n\n\n\n\n\n\n\n\n\n\n\n\n\nName:", self.name, "\nYear of Establishment:", self.year_of_estd, "\nCity:", self.city, "\nHighest Qualification:", self.highest_qualification, "\nYear of Joining:", self.year_of_joining, "\nName of Institute:", self.name_of_institute)
->>>>>>> d7d037c9f56c6e7da260f26ceb10ad9d3f4e42ff
 class peon(university):
     designation = "Peon"
     def __init__(self, name, year_of_estd, city, highest_qualification, year_of_joining, name_of_institute):
@@ -61,12 +42,7 @@ class peon(university):
         self.name_of_institute = name_of_institute
         super().__init__(name, year_of_estd, city)
     def display(self):
-<<<<<<< HEAD
-        os.system("clear")
-        print("Name:", self.name, "\nYear of Establishment:", self.year_of_estd, "\nCity:", self.city, "\nHighest Qualification:", self.highest_qualification, "\nYear of Joining:", self.year_of_joining, "\nName of Institute:", self.name_of_institute)
-=======
         print("\n\n\n\n\n\n\n\n\n\n\n\n\n\nName:", self.name, "\nYear of Establishment:", self.year_of_estd, "\nCity:", self.city, "\nHighest Qualification:", self.highest_qualification, "\nYear of Joining:", self.year_of_joining, "\nName of Institute:", self.name_of_institute)
->>>>>>> d7d037c9f56c6e7da260f26ceb10ad9d3f4e42ff
 
 
 opr = int(input("Welcome to Database System\nPress 1 to check Professor Database\nPress 2 to check Lab Assistant's Database\nPress 3 to check Office Assistant's Database\nPress 4 to check Peon's Database\n: "))
