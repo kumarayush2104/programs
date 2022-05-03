2a39b9e86d10f4517f2f690c42de2d2db61ac7b1
Ayush Kumar <40270440+kumarayush2104@users.noreply.github.com>
2022-05-03 22:48:50 +0530

    Create practical5.py

---
+++ practical5.py
@@ -0,0 +1,28 @@
+# Name: Ayush Kumar
+# Enrollment Number: 202103103510253
+# Program: B.Tech Computer Science and Engineering
+# Practical: 5
+
+# Declaring a String
+str = "Hello World"
+print(str)
+
+# lower()
+print(str.lower())
+
+# upper()
+print(str.upper())
+
+# join()
+lst = ["This", "is", "a", "string"]
+print(" ".join(lst))
+
+# split()
+lst = str.split(" ")
+print(lst)
+
+# find()
+print(str.find("llo"))
+
+# replace()
+print(str.replace("World", "Python"))
