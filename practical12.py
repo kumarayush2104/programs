d10e1438a028eaee6373ed937c84e40c040d84e4
Ayush Kumar <40270440+kumarayush2104@users.noreply.github.com>
2022-05-06 07:41:44 +0530

    Create practical12.py

---
+++ practical12.py
@@ -0,0 +1,20 @@
+# B.Tech Computer Science and Engineering
+# Ayush Kumar
+# 202103103510253
+
+class student:
+    std_name = None
+    std_age = None
+    std_branch = None
+    std_city = None
+    def get_data(self, name, age, branch, city):
+        self.std_name = name
+        self.std_age = age
+        self.std_branch = branch
+        self.std_city = city
+    def display(self):
+        print("Name:", self.std_name, "\nAge:", self.std_age, "\nBranch:", self.std_branch, "\nCity:", self.std_city)
+        
+std1 = student()
+std1.get_data("Ayush Kumar", "19", "B.tech CSE", "Surat")
+std1.display()
