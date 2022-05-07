2950ae79ee3acf019b6fc575d0561b6f18034386
Ayush Kumar <40270440+kumarayush2104@users.noreply.github.com>
2022-05-07 18:05:41 +0530

    Create practical22.py

---
+++ practical22.py
@@ -0,0 +1,10 @@
+# B.Tech Computer Science and Engineering
+# Ayush Kumar
+# 202103103510253
+
+try:
+    int(input("Enter a Integer: "))
+except ValueError:
+    print("Enter a valid Integer")
+finally:
+    print("Program ended !")
