0743c5a13b949504e19fddf14018230c30708685
Ayush Kumar <40270440+kumarayush2104@users.noreply.github.com>
2022-05-08 20:53:07 +0530

    New: Practical-3,11

---
+++ practical11.py
@@ -0,0 +1,11 @@
+# B.Tech Computer Science and Engineering
+# Ayush Kumar
+# 2021031035101253
+
+def add(*abr):
+	sum = 0
+	for i in abr:
+		sum = sum + i
+	return sum
+
+print(add(1,2,3))
---
+++ practical3.py
@@ -0,0 +1,20 @@
+# B.Tech Computer Science and Engineering
+# Ayush Kumar
+# 2021031035101253
+
+# Creation of tuple in python
+tu = (1,2,3,3,2,4,5,6)
+print(tu)
+
+# count(), counts the provided element
+print(tu.count(3))
+
+# index(), returns the index number of provided element
+print(tu.index(5))
+
+# Slicing experiments:
+# (0,1,2,3,4,5) : Positive Way indexing
+# (-6,-5,-4,-3,-2,-1) : Negative Way indexing
+# slicing operator=" : ", slicing=[Initial Index number: until Index number]
+
+print(tu[1:-1])
