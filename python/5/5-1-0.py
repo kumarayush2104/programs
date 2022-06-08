7e204e3b405891c1ce4487f2581e2e85b844eed8
Ayush Kumar <40270440+kumarayush2104@users.noreply.github.com>
2022-06-08 14:52:33 +0530

    Chapter 5 updated

---
+++ python/5/5-1-0.py
@@ -0,0 +1,40 @@
+print("Exception propagation in python")
+print("Here we have 4 keywords which offer as exceptions let's see")
+
+print("try: \n     try block")
+print("except: \n     except block")
+print("else: \n     else block")
+print("finally: \n     finally block")
+
+
+print("The question is when will they run ?")
+
+print("If try throws exception, except will run !!")
+print("If try does not throws exception, try and else block will run !!")
+print("\n\nFinally will run everytime !!")
+
+
+
+
+print("\n\nWith statement in python: ")
+print("with statement is use to create a scope with some instructions for it !")
+print("like, we can open file with: open('filename') and access it in anywhere in the code !")
+print("But through with statement we can restrict the access within a scope, (refer 5-5-1.py)")
+
+
+
+
+print("\n\nRaising of custom Exceptions (The raise keyword)")
+print("Do you want to create your own custom exceptions ? ")
+print("We have raise keyword for it. (refer 5-1-2.py)")
+
+
+
+print("Error checking strategies in Python !")
+print("Now we have 2 types of strategies for checking errors in python and they are:")
+
+print("LBYL (Look Before You Leap): As its abbreviation says, It checks the exceptions before running the rest instructions")
+
+print("EAFP (Easier to Ask Forgiveness Than Permission): It runs the code and then check for exceptions")
+
+print("For example refer 5-1-3.py")
